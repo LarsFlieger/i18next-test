3fa12e2e17726d94ca6b112e3ab2c63884d60b47
Lars Flieger <a.purenetwork@gmail.com>
2021-10-16 09:26:54 +0200

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import i18n from 'i18next'
+import { initReactI18next, useTranslation } from 'react-i18next'
+import React, { useState } from 'react';
+import TranslationEn from './lang/en/index';
+import TranslationDe from './lang/de/index';
 
-function App() {
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: TranslationEn
+      },
+      de: {
+        translation: TranslationDe
+      }
+    },
+    lng: 'en',
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false
+    }
+  })
+
+const App = () => {
+  const [x, setX] = useState(0)
+  const { t } = useTranslation()
+
+  const onChangeLanguage = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    i18n.changeLanguage(event.target.value)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <ul>
+            <li>{t("menu.HOME")}</li>
+            <li>{t("menu.PLAY")}</li>
+            <li>{t("menu.PLAYERS")}</li>
+            <li>{t("menu.PROFILE")}</li>
+          </ul>
+        </nav>
+        <p>{t("general.WELCOME", { count: x })}</p>
+        <p>{x}</p>
+        <button onClick={() => setX(x + 1)}>Click</button>
+        <select name="language" onChange={onChangeLanguage}>
+          <option value="en">English</option>
+          <option value="de">Deutsch</option>
+        </select>
       </header>
     </div>
   );
